a5951d5570b6df31cd98c052cc320f8e0b4b0bf3
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-07-08 10:36:29 -0300

    Adding posts in Angular

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -10,20 +10,25 @@ export class PostsService {
   private posts: Post[] = [];
   private postsSubject = new Subject<Post[]>();
   posts$ = this.postsSubject.asObservable();
+  private apiUrl = 'http://localhost:3000/api/posts';
 
   constructor(private httpClient: HttpClient) {}
 
   getPosts() {
-    this.httpClient.get<{ message: string; posts: Post[] }>(
-      'http://localhost:3000/api/posts'
-    ).subscribe(({posts}) => {
-      this.posts = posts;
-      this.postsSubject.next([...posts]);
-    });
+    this.httpClient
+      .get<{ message: string; posts: Post[] }>(this.apiUrl)
+      .subscribe(({ posts }) => {
+        this.posts = posts;
+        this.postsSubject.next([...posts]);
+      });
   }
 
   addPost(post: Post) {
-    this.posts = [...this.posts, post];
-    this.postsSubject.next(this.posts);
+    this.httpClient.post<{ message: string }>(this.apiUrl, post).subscribe((response) => {
+      console.log(response);
+      this.posts = [...this.posts, post];
+      this.postsSubject.next(this.posts);
+    })
+
   }
 }
