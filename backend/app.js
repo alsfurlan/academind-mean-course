6a073ac1a0a97502208379539bd7d643dfe9a7e2
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-07-08 09:32:18 -0300

    Fetching initial posts

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first middleware');
-  next();
-})
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    { id: "ashduasdhUASHd", title: "Post one", content: "Content of post one" },
+    { id: "ashduasdhUASHe", title: "Post two", content: "Content of post two" },
+    {
+      id: "ashduasdhUASHf",
+      title: "Post three",
+      content: "Content of post three",
+    },
+  ];
+  res.status(200).send({
+    message: "Posts fetched successfully",
+    posts,
+  });
+});
 
 app.use((req, res, next) => {
-  res.send('Hello from express!');
-})
+  res.send("Hello from express!");
+});
 
 module.exports = app;
