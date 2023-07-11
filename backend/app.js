05a90fb72894481f73f61699299eaa8c6d1d684a
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-07-11 18:33:58 -0300

    Connecting our node express app to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 const Post = require("./model/post");
 const app = express();
+
+mongoose.connect('mongodb+srv://max:Yfm4s5VawVNUzywk@academind.k0cse0j.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+  console.log('Connected to database!');
+}, () => {
+  console.log('Connection failed!')
+})
 
 app.use(bodyParser.json());
 
