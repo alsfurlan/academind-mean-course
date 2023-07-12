0000f9a04861ac80cb0df515b36f023be967ce92
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-07-12 18:14:10 -0300

    Fetching data from the database

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Post = require("./model/post");
 const app = express();
 
-mongoose.connect('mongodb+srv://max:Yfm4s5VawVNUzywk@academind.k0cse0j.mongodb.net/?retryWrites=true&w=majority')
-.then(() => {
-  console.log('Connected to database!');
-}, () => {
-  console.log('Connection failed!')
-})
+mongoose
+  .connect(
+    "mongodb+srv://max:Yfm4s5VawVNUzywk@academind.k0cse0j.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(
+    () => {
+      console.log("Connected to database!");
+    },
+    () => {
+      console.log("Connection failed!");
+    }
+  );
 
 app.use(bodyParser.json());
 
@@ -39,18 +45,11 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    { id: "ashduasdhUASHd", title: "Post one", content: "Content of post one" },
-    { id: "ashduasdhUASHe", title: "Post two", content: "Content of post two" },
-    {
-      id: "ashduasdhUASHf",
-      title: "Post three",
-      content: "Content of post three",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully",
-    posts,
+  Post.find().then(posts => {
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      posts,
+    });
   });
 });
 
