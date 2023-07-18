f2ccc9c2650717a4d05337ee0b67c72ede8763dc
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-07-18 02:23:40 -0300

    Adding post with an ID

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -18,9 +18,11 @@ export class PostsService {
     this.httpClient
       .get<{ message: string; posts: any }>(this.apiUrl)
       .pipe(
-        map(({ posts }) => posts.map(({ _id, ...post }) => ({ id: _id, ...post })))
+        map(({ posts }) =>
+          posts.map(({ _id, ...post }) => ({ id: _id, ...post }))
+        )
       )
-      .subscribe(posts => {
+      .subscribe((posts) => {
         this.posts = posts;
         this.postsSubject.next([...posts]);
       });
@@ -28,20 +30,19 @@ export class PostsService {
 
   addPost(post: Post) {
     this.httpClient
-      .post<{ message: string }>(this.apiUrl, post)
-      .subscribe((response) => {
-        console.log(response);
+      .post<{ message: string; postId: string }>(this.apiUrl, post)
+      .subscribe(({ postId }) => {
+        post.id = postId;
         this.posts = [...this.posts, post];
         this.postsSubject.next(this.posts);
       });
   }
 
   deletePost(postId: string) {
-    this.httpClient.delete(this.apiUrl + postId)
-      .subscribe(() => {
-        const updatedPosts = this.posts.filter(post => post.id !== postId);
-        this.posts = updatedPosts;
-        this.postsSubject.next(this.posts);
-      })
+    this.httpClient.delete(this.apiUrl + postId).subscribe(() => {
+      const updatedPosts = this.posts.filter((post) => post.id !== postId);
+      this.posts = updatedPosts;
+      this.postsSubject.next(this.posts);
+    });
   }
 }
