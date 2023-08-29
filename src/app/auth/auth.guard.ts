11e66a324152dc98376e454c4ba2b2a6c5ef1c90
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-08-29 20:15:47 -0300

    Adding route guards

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,32 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private route: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.authService.getAuthStateListener().pipe(
+      tap((auth) => {
+        if (!auth) {
+          this.route.navigate(['/login']);
+        }
+      })
+    );
+  }
+}
