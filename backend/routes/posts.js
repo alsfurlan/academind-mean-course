3af13cf1cca1c1858408a219a85d2c5768826775
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-09-04 19:48:35 -0300

    Protecting resources with authorization

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,38 +1,37 @@
-const express = require("express");
-const multer = require("multer");
+const express = require('express');
+const multer = require('multer');
 const router = express.Router();
-const checkAuth = require("../middlewares/auth");
-const Post = require("../models/post");
+const checkAuth = require('../middlewares/auth');
+const Post = require('../models/post');
 const fs = require('fs').promises;
 
 const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-  "image/gif": "gif",
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg',
+  'image/gif': 'gif',
 };
 
 const storage = multer.diskStorage({
-
   destination: (request, file, callback) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
     const error = null;
     if (!isValid) {
-      error = new Error("Invalid myme tipe");
+      error = new Error('Invalid myme tipe');
     }
-    callback(error, "backend/images");
+    callback(error, 'backend/images');
   },
   filename: (request, filename, callback) => {
-    const name = filename.originalname.toLowerCase().split(" ").join("-");
+    const name = filename.originalname.toLowerCase().split(' ').join('-');
     const extension = MIME_TYPE_MAP[filename.mimetype];
     callback(null, `${name}-${Date.now()}.${extension}`);
   },
 });
 
 router.post(
-  "",
+  '',
   checkAuth,
-  multer({ storage }).single("image"),
+  multer({ storage }).single('image'),
   (req, res, next) => {
     const post = new Post({
       title: req.body.title,
@@ -42,7 +41,7 @@ router.post(
     });
     post.save().then((post) => {
       res.status(201).json({
-        message: "Post added successfully!",
+        message: 'Post added successfully!',
         post: {
           id: post._id,
           title: post.title,
@@ -55,22 +54,22 @@ router.post(
   }
 );
 
-router.get("/:id", (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   Post.findById(req.params.id).then((post) => {
     if (post) {
       res.status(200).json({ post });
     } else {
-      res.status(404).json({ message: "Post not found!" });
+      res.status(404).json({ message: 'Post not found!' });
     }
   });
 });
 
-router.get("", (req, res, next) => {
+router.get('', (req, res, next) => {
   const pageSize = +req.query.pagesize || 10;
   const currentPage = +req.query.page || 0;
-  console.log("currentPage: ", currentPage);
-  console.log("pageSize: ", pageSize);
-  console.log("skip:", pageSize * currentPage);
+  console.log('currentPage: ', currentPage);
+  console.log('pageSize: ', pageSize);
+  console.log('skip:', pageSize * currentPage);
 
   let fetchedPosts;
   Post.find({})
@@ -82,31 +81,30 @@ router.get("", (req, res, next) => {
     })
     .then((count) => {
       res.status(200).json({
-        message: "Posts fetched successfully",
+        message: 'Posts fetched successfully',
         posts: fetchedPosts,
         maxPosts: count,
       });
     });
 });
 
-router.delete("/:id", checkAuth, (req, res, next) => {
+router.delete('/:id', checkAuth, (req, res, next) => {
   let postDeleted;
-  Post.findOneAndDelete({ _id: req.params.id })
-    .then((post) => {
-      postDeleted = post;
-      const {imagePath} = post;
-      const file = imagePath.substring(imagePath.indexOf('/images'));
-      return fs.rm(__dirname.replace('routes', '') + file);
-    })
-    .then(() => {
-      res.status(200).json({ message: "Post deleted!", postDeleted });
-    });
+  Post.deleteOne({ _id: req.params.id, creator: req.user.id }).then(
+    ({ deletedCount }) => {
+      if (deletedCount) {
+        res.status(200).json({ message: 'Post deleted!', postDeleted });
+      } else {
+        res.status(401).json({ messagee: 'Not authorized!' });
+      }
+    }
+  );
 });
 
 router.put(
-  "/:id",
+  '/:id',
   checkAuth,
-  multer({ storage }).single("image"),
+  multer({ storage }).single('image'),
   (req, res, next) => {
     const { title, content, id, image } = req.body;
     const imagePath = req.file ? getImagePath(req) : image;
@@ -117,17 +115,23 @@ router.put(
       imagePath,
       creator: req.user.id,
     });
-    Post.updateOne({ _id: req.params.id }, post).then(() => {
-      res.status(200).json({
-        message: "Update successful!",
-        post: { id, title, content, image: imagePath },
-      });
-    });
+    Post.updateOne({ _id: req.params.id, creator: req.user.id }, post).then(
+      ({ modifiedCount }) => {
+        if (modifiedCount) {
+          res.status(200).json({
+            message: 'Update successful!',
+            post: { id, title, content, image: imagePath },
+          });
+        } else {
+          res.status(401).json({ messagee: 'Not authorized!' });
+        }
+      }
+    );
   }
 );
 
 module.exports = router;
 
 function getImagePath(req) {
-  return `${req.protocol}://${req.get("host")}/images/${req.file.filename}`;
+  return `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
 }
